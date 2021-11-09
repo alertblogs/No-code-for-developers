a8f887c61890a45517e30dd090cb8e7b10f726f1
alertblogs <91723487+alertblogs@users.noreply.github.com>
2021-11-09 17:11:06 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ extensions = []
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = '.alertops'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'alertblogs'
+copyright = u'2021, www.AlertOps.com'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'AlertOps blogs'
 
 
 # -- Options for LaTeX output ---------------------------------------------
